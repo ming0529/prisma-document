a240cb03588c2e441d13fda7d0ba747a6a075f42
서민정 <121smmmj@gmail.com>
2023-09-19 18:34:02 +0900

    prisma-document

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { UserService } from './user/user.service';
+import { PostService } from './post/post.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, PrismaService, UserService, PostService],
+})
+export class AppModule {}
